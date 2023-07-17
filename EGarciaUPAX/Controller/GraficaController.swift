d55b070854dd629968d5c45d188f8d5bf62a8c2c
Emiliogn98 <Emiliogn.98@gmail.com>
2023-07-17 13:57:11 -0600

    antes de agregar valores

--- EGarciaUPAX/Controller/GraficaController.swift
+++ EGarciaUPAX/Controller/GraficaController.swift
@@ -13,14 +13,15 @@ class GraficaController: UIViewController {
     
     
     
-    @IBOutlet weak var pieChartView: UIView!
+    @IBOutlet weak var pieChartView: PieChartView!
     
     
-    @IBOutlet weak var pageControl: PieChartView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         get()
+        data()
         
 
     }
@@ -29,7 +30,34 @@ class GraficaController: UIViewController {
     @IBAction func pageControlAction(_ sender: UIPageControl) {
         
     }
+    func data(){
+        //  var dataEntries : [Values] = self.lista[0].values!
+        var dataEntries : [Double] = [30.0,20.0,10.0]
+     var dataEntriesArray : [PieChartDataEntry] = []
     
+//        for obj in dataEntries {
+//            let entry = PieChartDataEntry(value: dataEntries[obj], label: "Datos: ")
+//            dataEntriesArray.append(entry)
+//        }
+        for obj in 0..<dataEntries.count {
+                    let entry = PieChartDataEntry(value: dataEntries[Int(obj)], label: "Datos: ")
+                    dataEntriesArray.append(entry)
+                }
+        let dataSet = PieChartDataSet(entries: dataEntriesArray, label: "Datos")
+        let data = PieChartData(dataSet: dataSet)
+        
+        dataSet.colors = ChartColorTemplates.joyful()
+        dataSet.valueTextColor = .black
+        dataSet.valueFont = UIFont.systemFont(ofSize: 15)
+        pieChartView.legend.enabled = true
+        pieChartView.chartDescription.enabled = false
+        
+        pieChartView.data = data
+        
+        pieChartView.notifyDataSetChanged()
+        
+        
+    }
     func get(){
         GetViewModel.Get { response, error in
             if response?.data != nil {
